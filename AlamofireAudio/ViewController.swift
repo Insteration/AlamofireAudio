614059725ae4e9032f4110a4ec27abd4ba0c5eb0
Artem <inste@icloud.com>
2019-07-14 13:37:33 +0300

    Initial Commit

---
+++ AlamofireAudio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireAudio
+//
+//  Created by Артём Кармазь on 7/14/19.
+//  Copyright © 2019 Артём Кармазь. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
