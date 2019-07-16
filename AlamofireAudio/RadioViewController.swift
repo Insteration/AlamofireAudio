e53b4626cc580d31e7eeaadf44b09a1ebf5d121a
Artem <inste@icloud.com>
2019-07-16 19:35:17 +0300

    NC

--- AlamofireAudio/RadioViewController.swift
+++ AlamofireAudio/RadioViewController.swift
@@ -11,24 +11,34 @@ import Alamofire
 import AVKit
 
 class RadioViewController: UIViewController {
-
+    
+    let myButton: UIButton = {
+        var button = UIButton()
+        button.backgroundColor = .red
+        button.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        button.addTarget(self, action: #selector(createRadio), for: .touchUpInside)
+        button.setTitle("Play", for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        createRadio()
+        view.addSubview(myButton)
     }
     
-
-    func createRadio() {
+    @objc func createRadio() {
+        
         var player = AVAudioPlayer()
-        request(URL(string: "http://main.inf.fm:9103")!)
-            .stream { (data) in
-                do {
-                    player = try AVAudioPlayer(data: data, fileTypeHint: AVFileType.mp3.rawValue)
-                    player.prepareToPlay()
-                    player.play()
-                } catch {
-                    print(error.localizedDescription)
-                }
+        
+        request(URL(string: "http://main.inf.fm:9103")!).stream { (data) in
+            do {
+                player = try AVAudioPlayer(data: data, fileTypeHint: AVFileType.mp3.rawValue)
+                sleep(3)
+                player.prepareToPlay()
+                player.play()
+            } catch {
+                print(error.localizedDescription)
+            }
         }
     }
 }
